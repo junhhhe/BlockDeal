5de882211562faef9b3fc8a736c03a02ea65e3eb
yungwanseok <kang80224@naver.com>
2024-10-14 00:13:07 +0900

    수정한거

---
+++ src/main/java/SenierProject/BlockDeal/entity/Cart.java
@@ -0,0 +1,35 @@
+package SenierProject.BlockDeal.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Table(name = "tbl_cart")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "buyer_id")
+    private Member buyer;  // 장바구니를 소유한 구매자
+
+    @ManyToMany
+    @JoinTable(
+            name = "cart_product",
+            joinColumns = @JoinColumn(name = "cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products = new ArrayList<>();  // 장바구니에 담긴 상품들
+}
